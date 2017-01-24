399ed6a107b8970a3c3598b16cbc6e212f55b575
Rajesh Kumar <rajeshiitbhu2@gmail.com>
2017-01-24 21:58:23 +0530

    Fixed the error of server.js

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 import Server from 'socket.io';
 
-export default function startServer() {
+
+export default function startServer(store) {
   const io = new Server().attach(8090);
-  console.log('server started');
   store.subscribe(
     () => io.emit('state', store.getState().toJS())
   );
@@ -11,4 +11,5 @@ export default function startServer() {
     socket.emit('state', store.getState().toJS());
     socket.on('action', store.dispatch.bind(store));
   });
+  console.log('server started!!!');
 }
